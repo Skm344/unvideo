81f3640c7d88a113f2e709c7c308316762250231
Saleem <skm344@gmail.com>
2024-10-21 02:40:06 -0400

    Audio Mapping Works Here

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -189,33 +189,6 @@ const electronHandler = {
       .run();
   },
 
-  trimVideo: (filePath: string, outputFileName: string, ffmpegPath: string) => {
-    const outputDirectory = path.dirname(filePath);
-    const outputFilePath = path.join(outputDirectory, outputFileName);
-    // ffmpeg -ss 00:00:00 -to 00:06:28 -i input.mp4 -c copy output.mp4
-    try {
-      alert('ffmpeg filepath ' + ffmpegPath);
-      ffmpeg.setFfmpegPath(ffmpegPath);
-
-      ffmpeg(filePath)
-        .setStartTime('00:00:00')
-        .setDuration('00:06:28')
-        .output(outputFilePath)
-        .videoCodec('copy')
-        .audioCodec('copy')
-        .on('end', () => {
-          console.log('Processing finished successfully');
-          alert('yyoooo');
-        })
-        .on('error', (err) => {
-          console.error('Error: ' + err.message);
-          alert('error ' + err.message);
-        })
-        .run();
-    } catch (e: any) {
-      alert(JSON.stringify(e));
-    }
-  },
   ipcRenderer: {
     sendMessage(channel: Channels, ...args: unknown[]) {
       ipcRenderer.send(channel, ...args);
